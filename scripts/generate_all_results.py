ffe6047dcb5872ef15b79257ce594e6378a0d3dd
Nisarga <129542212+n1sarga@users.noreply.github.com>
2026-09-05 09:37:24 +0600

    Add one-command literature-aligned reproduction script

---
+++ scripts/generate_all_results.py
@@ -0,0 +1,7 @@
+from absolute_deviation.experiments import generate_all_results
+from absolute_deviation.plotting import generate_all_figures
+
+
+if __name__ == "__main__":
+    generate_all_results()
+    generate_all_figures()
